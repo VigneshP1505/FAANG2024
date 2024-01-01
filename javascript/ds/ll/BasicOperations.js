30ec675dbe6e8ae343f2d184f4f77907750124d4
Dheepika <rdheepikaamrita@gmail.com>
2024-01-01 16:38:47 -0500

    linked list basicops

---
+++ javascript/ds/ll/BasicOperations.js
@@ -0,0 +1,156 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
+
+  // add at th end of the list
+  append(data) {
+    const newNode = new Node(data);
+
+    if (this.head === null) {
+      this.head = newNode;
+    } else {
+      let currentNode = this.head;
+
+      while (currentNode.next !== null) {
+        currentNode = currentNode.next;
+      }
+
+      currentNode.next = newNode;
+    }
+
+    this.size++;
+  }
+
+  //add to the front
+  prepend(data) {
+    const newNode = new Node(data);
+
+    if (this.head === null) {
+      this.head = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+
+    this.size++;
+  }
+
+  //delete first
+  deleteFirst() {
+    if (this.head === null) {
+      throw new Error("No node to delete");
+    }
+
+    this.head = this.head.next;
+    this.size--;
+  }
+
+  //delete end
+  deleteEnd() {
+    if (this.head === null) {
+      throw new Error("No node to delete");
+    }
+
+    let currentNode = this.head;
+
+    while (currentNode?.next?.next != null) {
+      currentNode = currentNode.next;
+    }
+
+    currentNode.next = null;
+    this.size--;
+  }
+
+  printList() {
+    console.log("The list elements are:");
+    let currentNode = this.head;
+
+    while (currentNode != null) {
+      console.log(currentNode.data);
+      currentNode = currentNode.next;
+    }
+  }
+
+  insertAt(data, index = 0) {
+    if (index > this.size) {
+      throw new Error("index greater than size.");
+    }
+
+    if (index < 0) {
+      throw new Error("index less than 0");
+    }
+
+    const newNode = new Node(data);
+
+    if (this.head === null) {
+      this.head = newNode;
+    } else {
+      if (index === 0) {
+        this.prepend(data);
+      } else {
+        let currentNode = this.head;
+        let loopIndex = 0;
+
+        while (currentNode.next != null && loopIndex < index - 1) {
+          currentNode = currentNode.next;
+          loopIndex++;
+        }
+        newNode.next = currentNode.next;
+        currentNode.next = newNode;
+      }
+    }
+    this.size++;
+  }
+
+  deleteAt(index) {
+    if (index >= this.size) {
+      throw new Error("index greater than size");
+    }
+
+    if (index < 0) {
+      throw new Error("index less than 0");
+    }
+
+    if (index === 0) {
+      this.deleteFirst();
+    } else {
+      let currentNode = this.head;
+      let loopIndex = 0;
+      while (currentNode.next != null && loopIndex < index - 1) {
+        currentNode = currentNode.next;
+        loopIndex++;
+      }
+
+      currentNode.next = currentNode.next?.next;
+    }
+    this.size--;
+  }
+}
+
+const ll = new LinkedList();
+
+ll.append(1);
+ll.append(2);
+ll.append(3);
+ll.append(5);
+
+ll.printList();
+
+ll.insertAt(4, 3);
+ll.printList();
+ll.deleteAt(3);
+ll.printList();
+
+const ll2 = new LinkedList();
+ll2.insertAt(4);
+
+ll2.printList();
